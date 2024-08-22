a5956b2e4e34c1b7434680c0c5f0ceedd9713fb2
faridhash26 <55881466+faridhash26@users.noreply.github.com>
2024-08-22 11:49:23 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+composer install --no-dev --optimize-autoloader
 php artisan migrate --force || { echo 'Migration failed'; exit 1; }
 php artisan storage:link || { echo 'Storage link failed'; exit 1; }
 php artisan config:cache || { echo 'Config cache failed'; exit 1; }
