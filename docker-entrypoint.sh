8219e33aab7386a09d611c5ac17b4eac3ffd2a31
faridhash26 <55881466+faridhash26@users.noreply.github.com>
2024-08-23 05:48:12 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ php artisan storage:link || { echo 'Storage link failed'; exit 1; }
 php artisan config:cache || { echo 'Config cache failed'; exit 1; }
 php artisan route:cache || { echo 'Route cache failed'; exit 1; }
 
-exec apache2-foreground
+
